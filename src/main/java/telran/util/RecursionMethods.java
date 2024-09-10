5ab808ab50ff431eebe9acf569b83579c6acba5c
DaniilJustCode <gromdan90@gmail.com>
2024-09-10 17:18:05 +0300

    cw 27

--- src/main/java/telran/util/RecursionMethods.java
+++ src/main/java/telran/util/RecursionMethods.java
@@ -16,13 +16,69 @@ public class RecursionMethods {
         return n == 0 ? 1 : n * factorial(n - 1);
     }
 
-    public static long pow(int num, int degree) {
-        long res = 1;
-
+    public static long pow1(int num, int degree) {
         if (degree < 0) {
             throw new IllegalArgumentException();
         }
 
-        return res;
+        return degree == 0 ? 1 : num * pow(num, degree - 1);
+    }
+
+    /**
+     * 
+     * @param num - any integer number
+     * @param degree - any positive number
+     * @return - num ^ degree
+     * limitations:
+     * 1. no cycles allowed
+     * 2. arithmetic operators + ; - are allowed only
+     * bitwise operators like >>, <<, &&, etc disallowed
+     */
+    public static long pow(int num, int degree) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+ 
+    public static int sum(int[] array) {
+        return sum(array, array.length);
+    }
+
+    private static int sum(int[] array, int length) {
+        return length == 0 ? 0 : array[length - 1] + sum(array, length - 1);
+    }
+
+
+    /**
+     * 
+     * @param x
+     * @return x ^ 2
+     * limitations:
+     * 1. no cycles allowed
+     * 2. arithmetic operators + ; - are allowed only
+     * 3. no bitwise operators
+     * 4. no standart and additional methods are allowed
+     * 5. no additional fields of the class RecursionMethods are aloowed
+     */
+    public static int square(int x) {
+        //TODO
+        throw new UnsupportedOperationException();
+    }
+
+
+    /**
+     * 
+     * @param str
+     * @param subString
+     * @return true if subString is actually substring of the given string
+     * limitations
+     * 1. no cycles allowed
+     * 2. the allowed methods of class String are:
+     *  2.1 charAt(int index)
+     *  2.2 length()
+     *  2.3 substring(int beginIndex)
+     */
+    public static boolean isSubstring(String str, String subString) {
+        //TODO
+        throw new UnsupportedOperationException();
     }
 }
